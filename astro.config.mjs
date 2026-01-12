cf9dd265048a4221d831aa85adad10f519fc0f86
Hrag <hragmds@gmail.com>
2026-01-12 18:58:42 -0500

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'hragmds.github.io',
+  base: '/onlygoats-draft',
   vite: {
     ssr: {
       external: ['three']
