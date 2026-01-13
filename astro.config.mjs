31da0ae4c9f262fdb4fb79811f8e597e710ff4e0
Hrag <hragmds@gmail.com>
2026-01-12 19:04:45 -0500

    fixed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'hragmds.github.io',
+  site: 'https://hragmds.github.io',
   base: '/onlygoats-draft',
   vite: {
     ssr: {
